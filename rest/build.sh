4599275c83da07cd926ff81d2e020e28861f1eda
Dave Syer <dsyer@pivotal.io>
2019-02-20 12:06:33 +0000

    Also works with Spring WebClient

--- rest/build.sh
+++ rest/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mvn clean install
-native-image --enable-url-protocols=https,http -H:Name=target/demo -H:ReflectionConfigurationFiles=rest.json -H:IncludeResources='META-INF/services/.*' \
+native-image --enable-url-protocols=http -H:Name=target/demo -H:ReflectionConfigurationFiles=rest.json -H:IncludeResources='META-INF/services/.*' \
   --report-unsupported-elements-at-runtime --allow-incomplete-classpath \
   -H:DelayClassInitialization=io.netty.handler.codec.http.HttpClientCodec,io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder \
   -Dio.netty.noUnsafe=true -cp target/rest-1.0-SNAPSHOT.jar com.sample.App
--- rest/src/main/java/com/sample/App.java
+++ rest/src/main/java/com/sample/App.java
@@ -1,14 +1,20 @@
 package com.sample;
 
-import reactor.netty.http.client.HttpClient;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        HttpClient.create().baseUrl("http://google.com").get()
-                .responseSingle((response, content) -> content.asString())
-                .subscribe(System.out::println);
-        Thread.sleep(1000L);
+        CountDownLatch latch = new CountDownLatch(1);
+        WebClient.builder().baseUrl("http://google.com").build().get().retrieve()
+                .bodyToMono(String.class).subscribe(value -> {
+                    latch.countDown();
+                    System.out.println(value);
+                });
+        latch.await(1000L, TimeUnit.MILLISECONDS);
     }
 
 }
