6557256e5b61bfa9ec96a367f4550a79f495d83a
Dave Syer <dsyer@pivotal.io>
2019-02-20 18:20:36 +0000

    Simplify sample and remove dependency on Spring

--- config-props/src/main/java/com/sample/App.java
+++ config-props/src/main/java/com/sample/App.java
@@ -1,15 +1,32 @@
 package com.sample;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.annotation.AnnotationUtils;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         System.err.println(
                 Config.class.getAnnotation(ConfigurationProperties.class).value());
-        System.err.println(AnnotationUtils
-                .findAnnotation(Config.class, ConfigurationProperties.class).value());
+        System.err.println(((ConfigurationProperties) Proxy.newProxyInstance(
+                App.class.getClassLoader(),
+                new Class<?>[] { ConfigurationProperties.class }, new Interceptor()))
+                        .value());
+    }
+
+}
+
+class Interceptor implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return "blah";
     }
 
 }
@@ -26,5 +43,14 @@ class Config {
     public void setName(String name) {
         this.name = name;
     }
+
+}
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@interface ConfigurationProperties {
+
+    String value() default "";
 
 }
