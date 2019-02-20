9b793797963dc2808e3889aeb44382d84342de52
Dave Syer <dsyer@pivotal.io>
2019-02-20 16:58:50 +0000

    Add proxy.json configuration

--- config-props/build.sh
+++ config-props/build.sh
@@ -2,13 +2,13 @@
 
 mvn clean install
 native-image --no-server -H:Name=target/demo -H:ReflectionConfigurationFiles=config.json \
-  -H:IncludeResources='META-INF/services/.*|META-INF/spring.factories|application.properties' \
+  -H:IncludeResources='META-INF/spring.factories|application.properties' \
   --report-unsupported-elements-at-runtime --allow-incomplete-classpath \
-  -H:DelayClassInitialization=org.springframework.boot.SpringApplication\
+  -H:DynamicProxyConfigurationFiles=proxy.json \
   -cp target/config-props-1.0-SNAPSHOT.jar com.sample.App
 
 echo "In regular JVM"
 java -jar target/config-props-1.0-SNAPSHOT.jar
 echo ""
 echo "As GraalVM native image"
-./target/demo -Dorg.springframework.boot.logging.LoggingSystem=none
+./target/demo
