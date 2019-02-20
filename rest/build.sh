921e131bbe817d0d4074474b5b0bf49b6d588ab0
Dave Syer <dsyer@pivotal.io>
2019-02-20 10:43:14 +0000

    Working example from netty repo

---
+++ rest/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mvn clean install
+native-image -H:Name=target/demo -H:ReflectionConfigurationFiles=rest.json -H:IncludeResources='META-INF/services/.*' \
+  --report-unsupported-elements-at-runtime --allow-incomplete-classpath \
+  -H:DelayClassInitialization=io.netty.handler.codec.http.HttpClientCodec,io.netty.handler.codec.http.HttpObjectEncoder \
+  -Dio.netty.noUnsafe=true -cp target/rest-1.0-SNAPSHOT.jar com.sample.HttpSnoopClient
+
+echo "In regular JVM"
+java -jar target/rest-1.0-SNAPSHOT.jar
+echo ""
+echo "As GraalVM native image"
+./target/demo
