346689a70d75bb2ecc55b38359082d9d61d53af6
Dave Syer <dsyer@pivotal.io>
2019-04-03 15:29:46 +0100

    Fix functional sample (build flags)

--- functional-bean-definition/build.sh
+++ functional-bean-definition/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-mvn clean compile assembly:single
-native-image --allow-incomplete-classpath --delay-class-initialization-to-runtime=org.springframework.core.io.VfsUtils -H:ReflectionConfigurationFiles=graal.json -cp target/functional-bean-definition-1.0-SNAPSHOT-jar-with-dependencies.jar com.sample.App
+../mvnw clean package
+native-image --allow-incomplete-classpath --report-unsupported-elements-at-runtime --delay-class-initialization-to-runtime=org.springframework.core.io.VfsUtils -H:ReflectionConfigurationFiles=graal.json -cp target/functional-bean-definition-1.0-SNAPSHOT.jar com.sample.App
 
 echo "In regular JVM"
-java -cp target/functional-bean-definition-1.0-SNAPSHOT-jar-with-dependencies.jar com.sample.App
+java -cp target/functional-bean-definition-1.0-SNAPSHOT.jar com.sample.App
 echo ""
 echo "As GraalVM native image"
 ./com.sample.app
--- functional-bean-definition/src/main/java/com/sample/App.java
+++ functional-bean-definition/src/main/java/com/sample/App.java
@@ -1,17 +1,16 @@
 package com.sample;
 
-
 import org.springframework.context.support.GenericApplicationContext;
 
 public class App {
 
-    public static void main(String[] args) {
-        GenericApplicationContext context = new GenericApplicationContext();
-        context.registerBean(Foo.class);
-        context.registerBean(Bar.class);
-        context.refresh();
-        context.getBean(Foo.class);
-        context.getBean(Bar.class);
-    }
+	public static void main(String[] args) {
+		GenericApplicationContext context = new GenericApplicationContext();
+		context.registerBean(Foo.class, () -> new Foo());
+		context.registerBean(Bar.class);
+		context.refresh();
+		System.out.println("Foo: " + context.getBean(Foo.class));
+		System.out.println("Bar: " + context.getBean(Bar.class));
+	}
 
 }
